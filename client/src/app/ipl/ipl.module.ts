1313a36eefd671cce1a16cba6a912179ce429b30
Siroshma <dummy@gmail.com>
2026-03-15 09:13:43 +0000

    day20

--- client/src/app/ipl/ipl.module.ts
+++ client/src/app/ipl/ipl.module.ts
@@ -4,15 +4,41 @@ import { CommonModule } from "@angular/common";
 import { IplRoutingModule } from "./ipl-routing.module";
 import { ReactiveFormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
+import { TeamCreateComponent } from "./components/teamcreate/teamcreate.component";
+import { CricketerArrayComponent } from "./components/cricketerarray/cricketerarray.component";
+import { CricketerCreateComponent } from "./components/cricketercreate/cricketercreate.component";
+import { MatchCreateComponent } from "./components/matchcreate/matchcreate.component";
+
+import { VoteComponent } from './components/vote/vote.component';
+import { TicketBookingComponent } from './components/ticketbooking/ticketbooking.component';
 
 @NgModule({
-  declarations: [],
+ declarations: [
+    TeamCreateComponent,
+    CricketerArrayComponent,
+
+ VoteComponent,
+    TicketBookingComponent,
+
+ CricketerCreateComponent,
+    MatchCreateComponent
+
+  ],
   imports: [
     CommonModule,
     ReactiveFormsModule,
     HttpClientModule,
   ],
+
+
+
   exports: [
+      TeamCreateComponent,
+    CricketerArrayComponent,
+     CricketerCreateComponent,
+      VoteComponent,
+    TicketBookingComponent,
+    MatchCreateComponent
     
   ]
 })
